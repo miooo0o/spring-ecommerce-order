6de295f473eade23008b1aa5532989f8813f7219
Mina Kim <mina.kim@deliveryhero.com>
2025-08-09 23:29:14 +0200

    chore: remove unused methods

--- src/main/kotlin/ecommerce/repository/CartRepository.kt
+++ src/main/kotlin/ecommerce/repository/CartRepository.kt
@@ -2,13 +2,9 @@ package ecommerce.repository
 
 import ecommerce.model.Cart
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CartRepository : JpaRepository<Cart, Long> {
     fun findCartByMemberId(memberId: Long): Cart?
-
-    @Query("SELECT DISTINCT c FROM Cart c LEFT JOIN FETCH c.items")
-    fun findAllWithItems(): List<Cart>
 }
--- src/main/kotlin/ecommerce/repository/ProductRepository.kt
+++ src/main/kotlin/ecommerce/repository/ProductRepository.kt
@@ -1,10 +1,8 @@
 package ecommerce.repository
 
 import ecommerce.model.Product
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -13,12 +11,4 @@ interface ProductRepository : JpaRepository<Product, Long> {
 
     @Query("SELECT DISTINCT p FROM Product p LEFT JOIN FETCH p.options")
     fun findAllWithOptions(): List<Product>
-
-    @Query("SELECT p.id FROM Product p")
-    fun findIds(pageable: Pageable): List<Long>
-
-    @Query("SELECT DISTINCT p FROM Product p LEFT JOIN FETCH p.options WHERE p.id IN :ids")
-    fun findAllWithOptionsByIds(
-        @Param("ids") ids: List<Long>,
-    ): List<Product>
 }
