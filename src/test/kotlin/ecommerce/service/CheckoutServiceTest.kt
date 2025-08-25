242ab2f11e0c3f6c3a44ac83c697c5a46fd808ff
Mina Kim <mina.kim@deliveryhero.com>
2025-08-25 14:22:16 +0200

    chore: remove wildcard, make able to build

--- src/test/kotlin/ecommerce/service/CheckoutServiceTest.kt
+++ src/test/kotlin/ecommerce/service/CheckoutServiceTest.kt
@@ -1,21 +1,30 @@
 package ecommerce.service
 
 import ecommerce.client.StripeClient
-import ecommerce.dto.*
-import ecommerce.model.*
+import ecommerce.dto.CheckoutRequest
+import ecommerce.dto.PaymentIntent
+import ecommerce.dto.RegisteredMember
+import ecommerce.dto.Role
+import ecommerce.model.Option
+import ecommerce.model.Order
+import ecommerce.model.OrderItem
+import ecommerce.model.Payment
 import ecommerce.repository.PaymentRepository
 import org.apache.coyote.BadRequestException
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.*
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 
 @SpringBootTest
 class CheckoutServiceTest {
-
     @MockitoBean
     private lateinit var stripeClient: StripeClient
 
@@ -33,11 +42,12 @@ class CheckoutServiceTest {
         val member = RegisteredMember(1L, "test@email.com", Role.USER)
         val request = CheckoutRequest(1L, 1000L, "eur", "pm_card_visa")
 
-        val mockOrder = mock<Order> {
-            on { id } doReturn 1L
-            on { totalMinor } doReturn 1000L
-            on { items } doReturn mutableListOf()
-        }
+        val mockOrder =
+            mock<Order> {
+                on { id } doReturn 1L
+                on { totalMinor } doReturn 1000L
+                on { items } doReturn mutableListOf()
+            }
         val paymentIntent = PaymentIntent("pi_guri_cute_id", 1000L, "succeeded", "eur", "pm_card_visa", "secret", 123L)
         val payment = Payment(1000L, "eur", "pm_card_visa", "pi_guri_cute_id", "secret", "succeeded")
 
@@ -57,9 +67,10 @@ class CheckoutServiceTest {
         val member = RegisteredMember(1L, "test@email.com", Role.USER)
         val request = CheckoutRequest(1L, 2000L, "eur", "pm_card_visa")
 
-        val mockOrder = mock<Order> {
-            on { totalMinor } doReturn 1000L
-        }
+        val mockOrder =
+            mock<Order> {
+                on { totalMinor } doReturn 1000L
+            }
         whenever(orderService.createOrder(member.id)).thenReturn(mockOrder)
 
         assertThatThrownBy {
@@ -70,9 +81,16 @@ class CheckoutServiceTest {
     @Test
     fun `successfully executePayment`() {
         val request = CheckoutRequest(1L, 1500L, "eur", "pm_card_mastercard")
-        val paymentIntent = PaymentIntent("pi_guri_cute_id",
-            1500L, "requires_confirmation", "eur", "pm_card_mastercard", "secret", 16648303L,
-        )
+        val paymentIntent =
+            PaymentIntent(
+                "pi_guri_cute_id",
+                1500L,
+                "requires_confirmation",
+                "eur",
+                "pm_card_mastercard",
+                "secret",
+                16648303L,
+            )
         val savedPayment = Payment(1500L, "eur", "pm_card_mastercard", "pi_guri_cute_id", "secret", "requires_confirmation", id = 1L)
 
         whenever(stripeClient.makePayment(request)).thenReturn(paymentIntent)
@@ -90,18 +108,21 @@ class CheckoutServiceTest {
         val member = RegisteredMember(1L, "test@email.com", Role.USER)
         val request = CheckoutRequest(1L, 1000L, "eur", "pm_card_visa")
 
-        val mockOption = mock<Option> {
-            on { decreaseStock(2) } doReturn mock()
-        }
-        val mockOrderItem = mock<OrderItem> {
-            on { option } doReturn mockOption
-            on { quantity } doReturn 2
-        }
-        val mockOrder = mock<Order> {
-            on { id } doReturn 1L
-            on { totalMinor } doReturn 1000L
-            on { items } doReturn mutableListOf(mockOrderItem)
-        }
+        val mockOption =
+            mock<Option> {
+                on { decreaseStock(2) } doReturn mock()
+            }
+        val mockOrderItem =
+            mock<OrderItem> {
+                on { option } doReturn mockOption
+                on { quantity } doReturn 2
+            }
+        val mockOrder =
+            mock<Order> {
+                on { id } doReturn 1L
+                on { totalMinor } doReturn 1000L
+                on { items } doReturn mutableListOf(mockOrderItem)
+            }
 
         whenever(orderService.createOrder(member.id)).thenReturn(mockOrder)
         whenever(orderService.save(mockOrder)).thenReturn(mockOrder)
